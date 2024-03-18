4b03020eb442a6c17c937a651ce0f09a69fb5f9e
mwang <10364322+mwang0905@user.noreply.gitee.com>
2024-03-18 17:51:46 +0800

    代码路径，报错调整

--- ruoyi-solarProject/src/main/java/com/ruoyi/solarProject/util/ExportPPTUtil.java
+++ ruoyi-solarProject/src/main/java/com/ruoyi/solarProject/util/ExportPPTUtil.java
@@ -1,48 +1,12 @@
-package com.ruoyi.common.utils.poi;
-
-import com.ruoyi.common.annotation.Excel;
-import com.ruoyi.common.annotation.Excel.ColumnType;
-import com.ruoyi.common.annotation.Excel.Type;
-import com.ruoyi.common.annotation.Excels;
-import com.ruoyi.common.config.RuoYiConfig;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.core.text.Convert;
-import com.ruoyi.common.exception.UtilException;
-import com.ruoyi.common.utils.DateUtils;
-import com.ruoyi.common.utils.DictUtils;
-import com.ruoyi.common.utils.StringUtils;
-import com.ruoyi.common.utils.file.FileTypeUtils;
-import com.ruoyi.common.utils.file.FileUtils;
-import com.ruoyi.common.utils.file.ImageUtils;
-import com.ruoyi.common.utils.reflect.ReflectUtils;
+package com.ruoyi.solarProject.util;
 import com.ruoyi.solarProject.domain.PjBaseInfo;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.RegExUtils;
-import org.apache.commons.lang3.reflect.FieldUtils;
-import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.ooxml.POIXMLDocumentPart;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.ss.util.CellRangeAddressList;
-import org.apache.poi.util.IOUtils;
+import com.ruoyi.solarProject.domain.PjEnergySaving;
+import com.ruoyi.solarProject.domain.PjGenerProfitGather;
+import com.ruoyi.solarProject.domain.PjGenerProfitTest;
 import org.apache.poi.xslf.usermodel.*;
-import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-import org.apache.poi.xssf.usermodel.*;
-import org.openxmlformats.schemas.drawingml.x2006.spreadsheetDrawing.CTMarker;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * 导出PPT相关代码处理相关处理
@@ -68,16 +32,16 @@ public class ExportPPTUtil<T>
             setExcel1(slide1,pjBaseInfo);
             //第13页
             XSLFSlide slide2 = slides.get(12);
-            setExcel2(slide2);
+            setExcel2(slide2,pjBaseInfo);
             //第19页
             XSLFSlide slide3 = slides.get(18);
-            setExcel3(slide3);
+            setExcel3(slide3,null,null);
             //第19页
             XSLFSlide slide4 = slides.get(19);
-            setExcel4(slide4);
+            setExcel4(slide4,null);
             //第19页
             XSLFSlide slide5 = slides.get(20);
-            setExcel5(slide5);
+            setExcel5(slide5,null,null);
             fis.close();
             try (FileOutputStream out = new FileOutputStream("D:\\test1\\p55.pptx")) {
                 srcShow.write(out);
@@ -166,7 +130,8 @@ public class ExportPPTUtil<T>
 
     }
 
-    private static void setExcel2(XSLFSlide slide2) {
+    private static void setExcel2(XSLFSlide slide2,PjBaseInfo pjBaseInfo) {
+        PjGenerProfitGather pjGenerProfitGather = null;
         List<XSLFShape>   xslfShapes = slide2.getShapes();
         for (XSLFShape xslfShape : xslfShapes) {
             if(xslfShape instanceof XSLFTable){
@@ -174,126 +139,173 @@ public class ExportPPTUtil<T>
                 XSLFTable table = (XSLFTable) xslfShape;
                 XSLFTableCell cell = table.getCell(0,0);
                 //cell.setText("测试数据001");
-                //第二行第二列
+                //用电方---客户名称
                 cell = table.getCell(1,1);
-                cell.setText("11");
-                //第二行第四列
-                cell = table.getCell(1,3);
-                cell.setText("13");
-                //第二行第二列
-                cell = table.getCell(2,1);
-                cell.setText("21");
-                //第二行第四列
-                cell = table.getCell(2,3);
-                cell.setText("23");
-                //第二行第二列
-                cell = table.getCell(3,1);
-                cell.setText("31");
-                //第二行第四列
-                cell = table.getCell(3,3);
-                cell.setText("33");
+                cell.setText(pjBaseInfo.getCusName());
+                //投资金额
 
-                //第二行第二列
+                //pjTotalPrice/1000
+                cell = table.getCell(1,3);
+                cell.setText(String.valueOf(pjBaseInfo.getPjTotalPrice()));
+                //投资方
+                cell = table.getCell(2,1);
+                cell.setText(pjBaseInfo.getCusName());
+                //25年发电收益
+                cell = table.getCell(2,3);
+                cell.setText(String.valueOf(pjGenerProfitGather.getSumAnnulIncome()));
+                //装机容量
+                cell = table.getCell(3,1);
+                cell.setText(String.valueOf(pjBaseInfo.getTotalCapacity()));
+                //回本周期
+                cell = table.getCell(3,3);
+
+                //实时计算
+                cell.setText(String.valueOf("3.6"));
+
+                //25年发电量
                 cell = table.getCell(4,1);
-                cell.setText("41");
-                //第二行第四列
+
+                cell.setText(String.valueOf(pjGenerProfitGather.getTotalGenerate()));
+                //25年年均收益率
                 cell = table.getCell(4,3);
-                cell.setText("43");
+                cell.setText(String.valueOf(pjGenerProfitGather.getAvgIncomeRatioAnnul()));
 
             }
         }
 
 
     }
-    private static void setExcel3(XSLFSlide slide2) {
+    private static void setExcel3(XSLFSlide slide2, List<PjGenerProfitTest> pjGenerProfitTests,PjGenerProfitGather pjGenerProfitGather) {
         List<XSLFShape>   xslfShapes = slide2.getShapes();
         int i = 1 ;
         for (XSLFShape xslfShape : xslfShapes) {
             if(xslfShape instanceof XSLFTable){
 
                 if(i == 1){
-                    System.out.println("77777");
                     XSLFTable table = (XSLFTable) xslfShape;
                     XSLFTableCell cell = null;
-                    cell = table.getCell(2,1);
-                    System.out.println(cell);
-                    cell.setText("发电量"+11);
-
                     for(int g = 2 ; g < 15 ; g++){
-                        //第二行第二列
+                        PjGenerProfitTest pjGenerProfitTest = pjGenerProfitTests.get(g-2);
+                        //发电量
                         cell = table.getCell(g,1);
-                        cell.setText("发电量"+g);
+
+                        cell.setText(String.valueOf(pjGenerProfitTest.getAnnulGenerate()));
+                        //节省电费
                         cell = table.getCell(g,2);
-                        cell.setText("发电量"+g);
+                        cell.setText(String.valueOf(pjGenerProfitTest.getSaveElecPrice()));
+                        //余电上网
                         cell = table.getCell(g,3);
-                        cell.setText("发电量"+g);
+                        cell.setText(String.valueOf(pjGenerProfitTest.getSendStateIncome()));
+                        //年净收益
                         cell = table.getCell(g,4);
-                        cell.setText("发电量"+g);
+                        cell.setText(String.valueOf(pjGenerProfitTest.getAnnulIncome()));
                     }
                 }else{
-                    System.out.println("3333");
                     XSLFTable table = (XSLFTable) xslfShape;
                     XSLFTableCell cell = null;
                     for(int g = 2 ; g <= 15 ; g++){
-
                         //第二行第二列
                         cell = table.getCell(g,1);
                         if(cell == null){
                             continue;
                         }
-                        cell.setText("发电量"+g);
-                        cell = table.getCell(g,2);
-                        cell.setText("发电量"+g);
-                        cell = table.getCell(g,3);
-                        cell.setText("发电量"+g);
-                        cell = table.getCell(g,4);
-                        cell.setText("发电量"+g);
+                        PjGenerProfitTest pjGenerProfitTest = pjGenerProfitTests.get(g+11);
+                        if(pjGenerProfitTest == null){
+                            //设置合计的数量
+                            //发电量
+                            cell = table.getCell(g,1);
+
+                            cell.setText(String.valueOf(pjGenerProfitGather.getTotalGenerate()));
+                            //节省电费
+                            cell = table.getCell(g,2);
+                            cell.setText(String.valueOf(pjGenerProfitGather.getSumSavePrice()));
+                            //余电上网
+                            cell = table.getCell(g,3);
+                            cell.setText(String.valueOf(pjGenerProfitGather.getSumSendStateIncome()));
+                            //年净收益
+                            cell = table.getCell(g,4);
+                            cell.setText(String.valueOf(pjGenerProfitGather.getSumAnnulIncome()));
+
+
+
+
+
+                        }else{
+                            //发电量
+                            cell = table.getCell(g,1);
+
+                            cell.setText(String.valueOf(pjGenerProfitTest.getAnnulGenerate()));
+                            //节省电费
+                            cell = table.getCell(g,2);
+                            cell.setText(String.valueOf(pjGenerProfitTest.getSaveElecPrice()));
+                            //余电上网
+                            cell = table.getCell(g,3);
+                            cell.setText(String.valueOf(pjGenerProfitTest.getSendStateIncome()));
+                            //年净收益
+                            cell = table.getCell(g,4);
+                            cell.setText(String.valueOf(pjGenerProfitTest.getAnnulIncome()));
+                        }
+
+
+
                     }
                 }
                 if(i == 1){
                     i++;
                 }
-
-
-
             }
         }
 
 
     }
-    private static void setExcel4(XSLFSlide slide2) {
+    private static void setExcel4(XSLFSlide slide2, PjEnergySaving pjEnergySaving) {
         List<XSLFShape>   xslfShapes = slide2.getShapes();
         for (XSLFShape xslfShape : xslfShapes) {
             if(xslfShape instanceof XSLFTable){
                 System.out.println("2222");
                 XSLFTable table = (XSLFTable) xslfShape;
                 XSLFTableCell cell = null;
-                //第二行第二列
+                //年平均发电量
                 cell = table.getCell(1,2);
                 cell.setText("11");
-
+                //25年累计
                 cell = table.getCell(2,2);
                 cell.setText("11");
+                //25年累计
                 cell = table.getCell(3,2);
-                cell.setText("11");
+                long l1 = pjEnergySaving.getCoalSavingAverage();
+                cell.setText(String.valueOf(l1));
                 cell = table.getCell(4,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(l1*25));
+                //减排CO2
+
+                long carbonSavingAverage = pjEnergySaving.getCarbonSavingAverage();
+
                 cell = table.getCell(5,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(carbonSavingAverage));
                 cell = table.getCell(6,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(carbonSavingAverage*25));
+                //减排SO2
+                long sulfurSavingAverage = pjEnergySaving.getSulfurSavingAverage();
                 cell = table.getCell(7,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(sulfurSavingAverage));
                 cell = table.getCell(8,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(sulfurSavingAverage*25));
+
+                //nitricSavingAverage  减排氮氧化物
+
+                long nitricSavingAverage = pjEnergySaving.getNitricSavingAverage();
                 cell = table.getCell(9,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(nitricSavingAverage));
                 cell = table.getCell(10,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(nitricSavingAverage*25));
+
+                //nitricSavingAverage  减排氮氧化物
+                long smokeSavingAverage = pjEnergySaving.getSmokeSavingAverage();
                 cell = table.getCell(11,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(smokeSavingAverage));
                 cell = table.getCell(12,2);
-                cell.setText("11");
+                cell.setText(String.valueOf(smokeSavingAverage*25));
             }
         }
 
@@ -301,24 +313,44 @@ public class ExportPPTUtil<T>
     }
 
 
-    private static void setExcel5(XSLFSlide slide2) {
+    private static void setExcel5(XSLFSlide slide2,PjGenerProfitGather pjGenerProfitGather,PjBaseInfo pjBaseInfo) {
         List<XSLFShape>   xslfShapes = slide2.getShapes();
         for (XSLFShape xslfShape : xslfShapes) {
             if(xslfShape instanceof XSLFTable){
                 System.out.println("2222");
                 XSLFTable table = (XSLFTable) xslfShape;
                 XSLFTableCell cell = null;
-                //第二行第二列
+                //节省电费
                 cell = table.getCell(2,1);
-                cell.setText("11");
+                cell.setText(String.valueOf(pjGenerProfitGather.getSumSavePrice()));
+                //节省变压器容量
+                BigDecimal trancformerCapacity = pjBaseInfo.getTrancformerCapacity();
+               // 42*12*25*0.05/10000 == 0.063
+                BigDecimal b1 = new BigDecimal(0.063);
                 cell = table.getCell(3,1);
-                cell.setText("11");
+                cell.setText(trancformerCapacity.divide(b1).toString());
+                //温度节能
+                BigDecimal bigDecimal = pjBaseInfo.getRoofArea();
+                //客户国电网价格
+                BigDecimal bigDecimal3 = new BigDecimal(160);
+                BigDecimal electPrice = pjBaseInfo.getElectPrice();
+                BigDecimal electPrice1 = bigDecimal.divide(electPrice).multiply(bigDecimal3);
+
                 cell = table.getCell(4,1);
-                cell.setText("11");
+                cell.setText(electPrice1.toString());
+                //修缮费用   屋顶面积*50*2/10000
+
+                BigDecimal bigDecimal1 = new BigDecimal(100);
+                BigDecimal bigDecimal2 = bigDecimal.divide(bigDecimal1);
                 cell = table.getCell(5,1);
-                cell.setText("11");
+                cell.setText(bigDecimal2.toString());
+                //double s2 = 25*2.5/10000;  ==160
+
+                //碳交易收益
                 cell = table.getCell(6,1);
-                cell.setText("11");
+                //
+                cell.setText("22");
+                //合计
                 cell = table.getCell(7,1);
                 cell.setText("11");
             }
